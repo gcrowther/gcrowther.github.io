947529cafa2187c2686019307febb5c0e957b433
gcrowther <97800097+gcrowther@users.noreply.github.com>
2022-01-16 04:36:37 -0800

    Update script.js

--- script.js
+++ script.js
@@ -5,17 +5,17 @@ const yourScoreSpan = document.querySelector('[data-your-score]')
 const SELECTIONS = [
   {
     name: 'rock',
-    emoji: '✊',
+    emoji: '🐍',
     beats: 'scissors'
   },
   {
     name: 'paper',
-    emoji: '✋',
+    emoji: '🐸',
     beats: 'rock'
   },
   {
     name: 'scissors',
-    emoji: '✌',
+    emoji: '🐌',
     beats: 'paper'
   }
 ]
